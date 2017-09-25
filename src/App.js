1ec488b1b7ed25e4e947f6e4d108fc71332f96e4
Scott Serage <xttocsurge51@gmail.com>
2017-09-25 14:53:06 -0600

    Completed step 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import router from './router'
+import router from './router';
 
+import {connect} from 'react-redux';
 
 class App extends Component {
   constructor(){
@@ -149,4 +150,6 @@ class App extends Component {
   }
 }
 
-export default App;
+let mapStateToProps = state => state;
+
+export default connect(mapStateToProps)(App);
